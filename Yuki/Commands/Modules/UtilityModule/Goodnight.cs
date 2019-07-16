ca0d91934cd8842c6f934f7241570dee3b467ce7
Vee <Vee@DESKTOP-7QR4CK5>
2019-07-16 00:46:30 -0500

    Added [temporary implementation of] goodnight command

---
+++ Yuki/Commands/Modules/UtilityModule/Goodnight.cs
@@ -0,0 +1,40 @@
+﻿using Discord;
+using Qmmands;
+using System.Threading.Tasks;
+using Yuki.Core;
+
+namespace Yuki.Commands.Modules.UtilityModule
+{
+    public partial class UtilityModule
+    {
+        [Command("goodnight", "gn")]
+        public async Task GoodnightAsync()
+        {
+            string[] gn = new[]
+            {
+                "https://b.catgirlsare.sexy/B8fn.gif",
+                "https://b.catgirlsare.sexy/fm61.gif",
+                "https://b.catgirlsare.sexy/o7KS.gif",
+                "https://b.catgirlsare.sexy/qGeT.gif",
+                "https://b.catgirlsare.sexy/TIQN.gif",
+                "https://b.catgirlsare.sexy/zIIR.gif",
+                "https://b.catgirlsare.sexy/-sCU.gif",
+                "https://b.catgirlsare.sexy/sTEC.gif",
+                "https://b.catgirlsare.sexy/f6lx.gif",
+                "https://b.catgirlsare.sexy/2KwZ.gif",
+                "https://b.catgirlsare.sexy/MTWN.gif",
+                "https://b.catgirlsare.sexy/bYaW.gif",
+                "https://b.catgirlsare.sexy/t2-o.gif",
+                "https://b.catgirlsare.sexy/OWVo.gif",
+                "https://b.catgirlsare.sexy/_Zd-.gif",
+                "https://b.catgirlsare.sexy/4liQ.gif",
+                "https://b.catgirlsare.sexy/lrTe.gif"
+            };
+
+            EmbedBuilder embed = Context.CreateEmbedBuilder(Language.GetString("goodnight_title"))
+                .WithImageUrl(gn[new YukiRandom().Next(gn.Length)]);
+
+            await ReplyAsync(embed);
+        }
+    }
+}
