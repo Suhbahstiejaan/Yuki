e1e95dd1116bcda5680f1e5b1e9d7132b5f634de
Vee <Vee@DESKTOP-7QR4CK5>
2019-07-27 23:06:33 -0500

    Removed [unneeded] reset command

--- Yuki/Commands/Modules/OwnerModule/OwnerModule.cs
+++ Yuki/Commands/Modules/OwnerModule/OwnerModule.cs
@@ -4,16 +4,5 @@ using System.Threading.Tasks;
 namespace Yuki.Commands.Modules.OwnerModule
 {
     [Name("Owner")]
-    public partial class OwnerModule : YukiModule
-    {
-        [Command("reset")]
-        public async Task ResetAsync()
-        {
-            await Context.Client.CurrentUser.ModifyAsync(u =>
-            {
-                u.Username = "Yuki Dev";
-                u.Avatar = new Discord.Optional<Discord.Image?>(new Discord.Image(@"C:\Users\Vee\Downloads\b03d0723c43a9694d4ae92319fdea6a4.png"));
-            });
-        }
-    }
+    public partial class OwnerModule : YukiModule { }
 }
