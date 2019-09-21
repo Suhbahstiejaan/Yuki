20a0c47b32b5563b9599ea6ffb3799377702a9ee
Hunter Speaks <veenus224.7@gmail.com>
2019-09-21 10:36:50 -0500

    Levenshtein distance added to word filter

--- Yuki/Extensions/StringExtensions.cs
+++ Yuki/Extensions/StringExtensions.cs
@@ -70,62 +70,47 @@ namespace Yuki.Extensions
             return default;
         }
 
-        public static bool LevenshteinAny(this string s, string t)
+        public static int LevenshteinDistance(this string source1, string source2) //O(n*m)
         {
-            foreach(string str in s.Split(' '))
+            int source1Length = source1.Length;
+            int source2Length = source2.Length;
+            
+
+            int[,] matrix = new int[source1Length + 1, source2Length + 1];
+
+
+            // First calculation, if one entry is empty return full length
+            if (source1Length == 0)
             {
-                if(LevenshteinDistance(str.ToLower(), t.ToLower()) > 1)
+                return source2Length;
+            }
+
+            if (source2Length == 0)
+            {
+                return source1Length;
+            }
+
+            // Initialization of matrix with row size source1Length and columns size source2Length
+            for (int i = 0; i <= source1Length; matrix[i, 0] = i++) { }
+
+            for (int j = 0; j <= source2Length; matrix[0, j] = j++) { }
+
+
+            // Calculate rows and collumns distances
+            for (int i = 1; i <= source1Length; i++)
+            {
+                for (int j = 1; j <= source2Length; j++)
                 {
-                    return true;
+                    int cost = (source2[j - 1] == source1[i - 1]) ? 0 : 1;
+
+                    matrix[i, j] = Math.Min(
+                        Math.Min(matrix[i - 1, j] + 1, matrix[i, j - 1] + 1),
+                                    matrix[i - 1, j - 1] + cost);
                 }
             }
 
-            return false;
-        }
-
-        public static int LevenshteinDistance(this string s, string t)
-        {
-            int n = s.Length;
-            int m = t.Length;
-            int[,] d = new int[n + 1, m + 1];
-
-            // Step 1
-            if (n == 0)
-            {
-                return m;
-            }
-
-            if (m == 0)
-            {
-                return n;
-            }
-
-            // Step 2
-            for (int i = 0; i <= n; d[i, 0] = i++)
-            {
-            }
-
-            for (int j = 0; j <= m; d[0, j] = j++)
-            {
-            }
-
-            // Step 3
-            for (int i = 1; i <= n; i++)
-            {
-                //Step 4
-                for (int j = 1; j <= m; j++)
-                {
-                    // Step 5
-                    int cost = (t[j - 1] == s[i - 1]) ? 0 : 1;
-
-                    // Step 6
-                    d[i, j] = Math.Min(
-                        Math.Min(d[i - 1, j] + 1, d[i, j - 1] + 1),
-                        d[i - 1, j - 1] + cost);
-                }
-            }
-            // Step 7
-            return d[n, m];
+            // return result
+            return matrix[source1Length, source2Length];
         }
     }
 }
