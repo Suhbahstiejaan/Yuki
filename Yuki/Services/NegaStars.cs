9ec33bbd499c32fe01641b64f0621e141c1bcf89
VeeDeeOh <veenus224.7@gmail.com>
2020-02-04 12:32:59 -0600

    Removed guild check for NegaStar

--- Yuki/Services/NegaStars.cs
+++ Yuki/Services/NegaStars.cs
@@ -13,19 +13,14 @@ namespace Yuki.Services
 
         public static async void Manage(IUserMessage message, ITextChannel channel, SocketReaction reaction)
         {
-            if((channel as IGuildChannel).GuildId != 267732080564240395 && (channel as IGuildChannel).GuildId != 620246094756184064)
-            {
-                return;
-            }
-
             IGuild guild = (channel as IGuildChannel).Guild;
 
             IGuildUser user = await guild.GetUserAsync(message.Author.Id);
 
             GuildConfiguration config = GuildSettings.GetGuild(guild.Id);
 
-            if(!config.Equals(default(GuildConfiguration)) && config.EnableNegaStars && !reaction.User.Value.IsBot
-               && config.NegaStarIgnoredChannels != null && !config.NegaStarIgnoredChannels.Contains(message.Channel.Id))
+            if(!config.Equals(default(GuildConfiguration)) && config.EnableNegaStars && !reaction.User.Value.IsBot &&
+                config.NegaStarIgnoredChannels != null && !config.NegaStarIgnoredChannels.Contains(message.Channel.Id))
             {
                 int negaCount = message.Reactions.Keys.Select(r => r.Name == Emote) != null ? message.Reactions.FirstOrDefault(r => r.Key.Name == Emote).Value.ReactionCount : 0;
                 
